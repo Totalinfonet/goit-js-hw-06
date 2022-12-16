e75edf064dc1b6b6a8dcd413f6c645b52f1d1aec
Totalinfonet <totalinfonet@gmail.com>
2022-12-17 00:07:17 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const tagRef = document.querySelector("#ingredients");
+
+const listElements = ingredients.map((ingredient) => {
+  const itemEl = document.createElement("li");
+  itemEl.textContent = ingredient;
+  itemEl.classList.add("item");
+  return itemEl;
+});
+
+console.log(listElements);
+
+tagRef.append(...listElements);
