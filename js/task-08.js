4f16dd7b75edb76f1cda2897a95abe96a761d171
Totalinfonet <totalinfonet@gmail.com>
2022-12-19 23:49:27 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const formRef = document.querySelector(".login-form");
+formRef.addEventListener("submit", onSubmit);
 
+function onSubmit(event) {
+  event.preventDefault();
+
+  const email = formRef.elements.email;
+  const password = formRef.elements.password;
+
+  if (email.value === "" || password.value === "") {
+    alert("Please fill in all the fields!");
+  } else {
+    const formData = {
+      email: email.value,
+      password: password.value,
+    };
+    console.log(formData);
+    formRef.reset();
+  }
+}
